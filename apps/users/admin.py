731ebe7ccc03d9a17aa078df82bd032c8cb763d8
Malei666 <826908021@qq.com>
2019-11-04 22:35:57 +0800

    19-11-04更新 dashboard样式

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,6 +3,7 @@
 ######################################
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.models import LogEntry
 
 ######################################
 # 自己写的模块
@@ -11,6 +12,7 @@ from .models import *
 
 
 class UserAdmin(UserAdmin):
+
     # fieldsets=()
     fieldsets = (
         ( '用户信息',{
@@ -33,12 +35,23 @@ class UserAdmin(UserAdmin):
 
             )
         }),
+        ('用户权限',{
+            'fields':(
+                'groups',
+                'user_permissions'
+            )
+        }),
         ('用户密码', {
             'fields': (
                 'password',
             )
         }),
     )
+    list_display = ('id','username','user_name', 'role','gender','unit_name','dept_name','mobile','email', 'status', 'create_time')
+    list_filter = ('is_staff', 'gender', 'is_active', 'groups')
+    search_fields = ('username','user_name','unit_name', 'dept_name', 'mobile')
+    ordering = ('role',)
+
 
 
 ######################################
@@ -49,3 +62,5 @@ admin.site.register(UserDepartment)
 admin.site.register(UserProfile, UserAdmin)
 admin.site.register(UserEmailVirificationCode)
 admin.site.register(UserLoginInfo)
+
+admin.site.register(LogEntry)
