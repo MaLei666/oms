8689a330566963fddb74a5fdb73f0c2bf4a6c512
Malei666 <826908021@qq.com>
2020-02-27 19:02:33 +0800

    20-02-27更新 完成user权限接口及测试，添加系统管理模块，添加主机管理模块接口

--- apps/document_management/migrations/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/home/zkfr/.local/share/virtualenvs/xf-5EfV3Nly/bin/python
-#-*- coding:utf-8 -*-
-# @author : MaLei 
-# @datetime : 2020-02-27 18:49
-# @file : __init__.py.py
-# @software : PyCharm
